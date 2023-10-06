aceb9b7c2e4bd33ea640460f165db87c7822e0c1
Gabriel Mansilla <gabriel001mansilla@gmail.com>
2023-10-06 14:33:35 -0300

    viendo el metodo get

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from fastapi import FastAPI #importamos a la clase fastapi que define nuestra app
+from fastapi import FastAPI 
 
-app = FastAPI() #definimos este objeto que va a llamar al constructor de la clase fastapi
+app = FastAPI() 
 
-#luego a este objeto le agregamos las rutas que queremos manejar con esta app
-#para ello llamamos el decorador app con una ruta
+@app.get("/items/{item_id}")
 
-@app.get("/")
-
-#es una buena practica en general definir una ruta con un mensaje de bienvenida en la direccion /
-
-#luego definimos una funcion
-
-def read_root():
-    return {"mesaje":"Hola comunidad Humai"}
+def read_item(item_id: int):
+    return {"item_id":item_id}
